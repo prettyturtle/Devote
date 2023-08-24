3f641f20d14bc60f0ffffd2ee0a891157eadb3c8
prettyturtle <unifconti@gmail.com>
2023-08-25 02:05:50 +0900

    Initial Commit

---
+++ yc_Devote/yc_DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  yc_DevoteApp.swift
+//  yc_Devote
+//
+//  Created by yc on 2023/08/25.
+//
+
+import SwiftUI
+
+@main
+struct yc_DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

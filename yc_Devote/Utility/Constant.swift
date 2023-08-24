401e0eb282f53170b791d2ddaf46833d2df40938
prettyturtle <unifconti@gmail.com>
2023-08-25 02:42:41 +0900

    SetUp Project

---
+++ yc_Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  yc_Devote
+//
+//  Created by yc on 2023/08/25.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
--- yc_Devote/yc_DevoteApp.swift
+++ yc_Devote/yc_DevoteApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct yc_DevoteApp: App {
     let persistenceController = PersistenceController.shared
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()

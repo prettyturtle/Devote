7a29334636a6c791e348637cd7477cf0a7b92286
prettyturtle <unifconti@gmail.com>
2023-08-26 03:39:45 +0900

    Widget

---
+++ yc_DevoteWidget/yc_DevoteWidget.swift
@@ -0,0 +1,119 @@
+//
+//  yc_DevoteWidget.swift
+//  yc_DevoteWidget
+//
+//  Created by yc on 2023/08/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct yc_DevoteWidgetEntryView : View {
+    var entry: Provider.Entry
+    
+    @Environment(\.widgetFamily) var widgetFamily
+    
+    var fontStyle: Font {
+        if widgetFamily == .systemSmall {
+            return .system(.footnote, design: .rounded)
+        } else {
+            return .system(.headline, design: .rounded)
+        }
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                backgroundGradient
+                
+                Image("rocket-small")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("logo")
+                    .resizable()
+                    .frame(
+                        width: widgetFamily != .systemSmall ? 56 : 36,
+                        height: widgetFamily != .systemSmall ? 56 : 36
+                    )
+                    .offset(x: (geometry.size.width / 2) - 20, y: (geometry.size.height / -2) + 20)
+                    .padding(.top, widgetFamily != .systemSmall ? 32 : 12)
+                    .padding(.trailing, widgetFamily != .systemSmall ? 32 : 12)
+                
+                HStack {
+                    Text("Just Do It")
+                        .foregroundColor(.white)
+                        .font(fontStyle)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 4)
+                        .background(.black.opacity(0.5).blendMode(.overlay))
+                        .clipShape(Capsule())
+                    
+                    if widgetFamily != .systemSmall {
+                        Spacer()
+                    }
+                }
+                .padding()
+                .offset(y: (geometry.size.height / 2) - 24)
+            }
+        }
+    }
+}
+
+struct yc_DevoteWidget: Widget {
+    let kind: String = "yc_DevoteWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            yc_DevoteWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Devote Launcher")
+        .description("This is an example widget for the personal task manager app.")
+    }
+}
+
+struct yc_DevoteWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            yc_DevoteWidgetEntryView(entry: SimpleEntry(date: Date()))
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+            
+            yc_DevoteWidgetEntryView(entry: SimpleEntry(date: Date()))
+                .previewContext(WidgetPreviewContext(family: .systemMedium))
+            
+            yc_DevoteWidgetEntryView(entry: SimpleEntry(date: Date()))
+                .previewContext(WidgetPreviewContext(family: .systemLarge))
+        }
+    }
+}
---
+++ yc_DevoteWidget/yc_DevoteWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  yc_DevoteWidgetBundle.swift
+//  yc_DevoteWidget
+//
+//  Created by yc on 2023/08/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct yc_DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        yc_DevoteWidget()
+        yc_DevoteWidgetLiveActivity()
+    }
+}
